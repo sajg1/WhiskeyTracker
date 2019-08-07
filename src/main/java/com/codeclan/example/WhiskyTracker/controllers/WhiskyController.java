aefcc64ea7844f86b260d64d8430add3833fd97e
Steve Gossip <stephengossip@gmail.com>
2019-08-07 19:05:16 +0100

    wrote tests for creating a distillery and a whisky, both passing

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,5 +1,6 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,7 +1,12 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,28 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class WhiskyTrackerApplicationTests {
 
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void createDistillery() {
+		Distillery distillery1 = new Distillery("Glendronach", "Highland");
+		distilleryRepository.save(distillery1);
+	}
+
+	@Test
+	public void createWhisky() {
+		Distillery distillery1 = new Distillery("Glendronach", "Highland");
+		distilleryRepository.save(distillery1);
+		Whisky whisky1 = new Whisky("The Glendronach Revival", 15, 2018, distillery1);
+		whiskyRepository.save(whisky1);
 	}
 
 }
